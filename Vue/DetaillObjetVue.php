a782f64db0a787e60ee17e69b640ef152ef722fc
Maxence Lissonnet <maxencel86@outlook.fr>
2025-11-25 13:13:43 +0100

    Ajout du bouton signaler + redirection maps

--- Vue/DetaillObjetVue.php
+++ Vue/DetaillObjetVue.php
@@ -5,11 +5,11 @@
 <div class="detail-container">
     <img src="/Annee_2_dev_web/SAE-S3/Controller/ImageObjet.php?id=<?php echo (int)$article['idObjet']; ?>" alt="">
     <div class="detail-card">
-        <h1 class='position-signaler'> <div class='titre-objet'> <?php echo htmlspecialchars($article['nomObjet']); ?> <div class="date">- <?php echo htmlspecialchars($article['dateDispoObjet']); ?> </div> </div> <a href="/Annee_2_dev_web/SAE-S3/Controller/SignalerController.php"> <button class="bouton-signaler"><img src="../Asset/image/CatalogueArticle/logo white flag.png" class="signaler"></button> </a></h1>
+        <h1 class='position-signaler'> <div class='titre-objet'> <?php echo htmlspecialchars($article['nomObjet']); ?> <div class="date">- <?php echo htmlspecialchars($article['dateDispoObjet']); ?> </div> </div> <img src="../Asset/image/CatalogueArticle/logo white flag.png" class="signaler"></button> </a></h1>
         <div class="divider"></div>
 
         <p class="logo"><img src="../Asset/image/CatalogueArticle/logo personne.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['nomUser']) . ' ' . htmlspecialchars($article['prenomUser']); ?></span></p>
-        <p class="logo"><img src="../Asset/image/CatalogueArticle/logo point maps.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['adresseLieuRetrait']); ?></span> </p>
+        <p class="logo"><img src="../Asset/image/CatalogueArticle/logo point maps.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['adresseLieuRetrait']); ?></span> <a class="itineraireImg" href="https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=<?php echo htmlspecialchars($article['coordonneesLieuRetrait']); ?>"><img id="logo-icon" src="../Asset/image/itineraire.png"></a></p>
         <p class="logo"><img src="../Asset/image/CatalogueArticle/logo shapes.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['nomCategorie']); ?></span></p>
         <p class="logo"><img src="../Asset/image/CatalogueArticle/logo mesure.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['mesureObjet']); ?></span></p>
         <p class="logo"><img src="../Asset/image/CatalogueArticle/logo etat.png" class="logo-icon"><span class="text"><?php echo htmlspecialchars($article['nomEtatObjet']); ?></span></p>
